1646f6a4d3f31597f346dc7235d12359967c5761
arvie <ctechnology24@gmail.com>
2017-10-15 06:42:45 +0800

    rebirth db host value

--- stories/settings/production.py
+++ stories/settings/production.py
@@ -128,7 +128,7 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USERNAME'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': '',
+        'HOST': os.environ.get('DB_HOST'),
     }
 }
 
